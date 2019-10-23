7dda0b01c4b48b27eaba36917e133d7c653e1fc5
Ozgur Ozcitak <ozcitak@yahoo.com>
2019-10-23 17:37:14 +0300

    Export URLRecord

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { URLAlgorithm } from './URLAlgorithm'
 export { URLImpl as URL } from './URLImpl'
 export { URLSearchParamsImpl as URLSearchParams } from './URLSearchParamsImpl'
+export { URLRecordInternal as URLRecord } from './interfacesInternal'
